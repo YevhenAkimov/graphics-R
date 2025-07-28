142fb4d11e51db65a902f7ace1288f792b52737a
YevhenAkimov <52507019+YevhenAkimov@users.noreply.github.com>
2025-07-28 11:29:15 +0300

    Create colors.R

---
+++ colors.R
@@ -0,0 +1,97 @@
+requie(paletteer)
+requie(ggthemes)
+colorscheme_prism   = c('#5F4690','#1D6996','#38A6A5','#0F8554','#73AF48','#EDAD08','#E17C05','#CC503E','#94346E','#6F4070','#994E95','#666666')
+colorscheme_ant     = c('#855C75','#D9AF6B','#AF6458','#736F4C','#526A83','#625377','#68855C','#9C9C5E','#A06177','#8C785D','#467378','#7C7C7C')
+colorscheme_ant_mod = c('#D9AF6B','#736F4C','#526A83','#625377','#68855C','#9C9C5E','#A06177','#8C785D','#467378','#7C7C7C')
+colorscheme_pastel  = c('#66C5CC','#F6CF71','#F89C74','#DCB0F2','#87C55F','#9EB9F3','#FE88B1','#C9DB74','#8BE0A4','#B497E7','#D3B484','#B3B3B3')
+colorscheme_safe    = c('#88CCEE','#CC6677','#DDCC77','#117733','#332288','#AA4499','#44AA99','#999933','#882255','#661100','#6699CC','#888888') 
+
+colorscheme_prism_mod. = c('#5F4690','#1D6996','#38A6A5','#0F8554','#73AF48','#EDAD08','#E17C05','#CC503E','#94346E','#F49FBC','#1C110A','#93A8AC')
+colorscheme_prism_mod3 = c('#5F4690','#1D6996','#0F8554','#EDAD08','#CC503E','#94346E','#1C1F33','#93A8AC','#A16928','#065256')
+colorscheme_prism_mod2 = c('#5F4690','#1D6996','#38A6A5','#0F8554','#73AF48','#EDAD08','#E17C05','#CC503E','#94346E','#F49FBC','#1C1F33','#93A8AC',"#C9DB74","#A16928")
+colorscheme_safe_mod   = c('#66C5CC','#4E4187',"#1C1F33",'#0F8554','#D17B88','#D58936','#065256','#999933','#882255','#a059a0','#888888') 
+colorscheme_tab20 = c("#ebce2b", "#702c8c", "#db6917", "#96cde6", "#ba1c30", "#c0bd7f", "#7f7e80", "#5fa641", "#d485b2", "#4277b6", "#df8461", "#463397", "#e1a11a", "#91218c", "#e8e948", "#7e1510", "#92ae31", "#6f340d", "#d32b1e", "#2b3514")
+
+
+colorscheme_prism_6colors   = c('#5F4690','#1D6996','#EDAD08','#CC503E','#94346E','#0F8554')
+colorscheme_prism_5colors   = c('#1D6996','#0F8554','#E17C05','#94346E','#666666')
+
+palettes <- ggthemes::ggthemes_data[["tableau"]][["color-palettes"]][["regular"]]
+colorscheme_Tableau20=palettes[["Tableau 20"]]$value
+colorscheme_Tableau10=palettes[["Tableau 10"]]$value
+colorscheme_Classic_BlueRed12 = palettes[["Classic Blue-Red 12"]]$value
+colorscheme_Classic_Cyclic = palettes[["Classic Cyclic"]]$value
+colorscheme_Jewel_Bright = palettes[["Jewel Bright"]]$value
+colorscheme_Summer = palettes[["Summer"]]$value
+colorscheme_GreenOrangeTeal = palettes[["Green-Orange-Teal"]]$value
+colorscheme_RedBlueBrown = palettes[["Red-Blue-Brown"]]$value
+colorscheme_Winter = palettes[["Winter"]]$value
+colorscheme_Hue_Circle = palettes[["Hue Circle"]]$value
+colorscheme_Classic10 = palettes[["Classic 10"]]$value
+colorscheme_Classic20 = palettes[["Classic 20"]]$value
+colorscheme_Classic10Medium = palettes[["Classic 10 Medium"]]$value
+colorscheme_Miller_Stone = ggthemes::ggthemes_data[["tableau"]][["color-palettes"]][["regular"]][["Miller Stone"]]$value
+colorscheme_Superfishel_Stone = palettes[["Superfishel Stone"]]$value
+
+
+
+cls=c("#1C1F33","#4E4187","#9e0142")
+Teal        = c('#d1eeea','#a8dbd9','#85c4c9','#68abb8','#4f90a6','#3b738f','#2a5674')
+Teal_mod        = c('#68abb8','#4f90a6','#3b738f','#2a5674')
+Teal_mod2        = c('#4f90a6','#3b738f','#2a5674',"#133d54")
+SunsetDark_mod=c('#e34f6f','#dc3977','#b9257a','#7c1d6f')
+Mint_mod=       c("#f9fff8",'#e4f1e1','#b4d9cc','#89c0b6','#63a6a0','#448c8a','#287274','#0d585f',"#065256","#013838")
+DarkMint_mod= c('#7bbcb0','#559c9e','#3a7c89','#235d72','#123f5a')
+RedOr       = c('#f6d2a9','#f5b78e','#f19c7c','#ea8171','#dd686c','#ca5268','#b13f64')
+BluGrn =    c('#c4e6c3','#96d2a4','#6dbc90','#4da284','#36877a','#266b6e','#1d4f60')
+Mint=       c('#e4f1e1','#b4d9cc','#89c0b6','#63a6a0','#448c8a','#287274','#0d585f')
+BurgYi =    c('#fbe6c5','#f5ba98','#ee8a82','#dc7176','#c8586c','#9c3f5d','#70284a')
+DarkMint =  c('#d2fbd4','#a5dbc2','#7bbcb0','#559c9e','#3a7c89','#235d72','#123f5a')
+Purp =      c('#f3e0f7','#e4c7f1','#d1afe8','#b998dd','#9f82ce','#826dba','#63589f')
+Sunset =    c('#f3e79b','#fac484','#f8a07e','#eb7f86','#ce6693','#a059a0','#5c53a5')
+SunsetDark= c('#fcde9c','#faa476','#f0746e','#e34f6f','#dc3977','#b9257a','#7c1d6f')
+Fall =      c('#3d5941','#778868','#b5b991','#f6edbd','#edbb8a','#de8a5a','#ca562c')
+Geyser =    c('#008080','#70a494','#b4c8a8','#f6edbd','#edbb8a','#de8a5a','#ca562c')
+Temps =     c('#009392','#39b185','#9ccb86','#e9e29c','#eeb479','#e88471','#cf597e')
+Temps2 =     c('#39b185','#9ccb86','#fffeeb','#eeb479','#e88471')
+Earth =     c('#A16928','#bd925a','#d6bd8d','#edeac2','#b5c8b8','#79a7ac','#2887a1')
+Greys= c('#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#000000')
+Greys1.5= c('#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#000000')
+Greys2= c('#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525')
+Greys2.5= c('#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525')
+Greys3= c('#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525')
+Greys4= c('#f9f9f9','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525')
+Greys5= c('#f9f9f9','#d9d9d9','#bdbdbd','#969696','#737373','#525252')
+Greys6= c('#f0f0f0','#f9f9f9','#d9d9d9','#bdbdbd','#969696','#737373','#525252')
+Spectral= c("#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2")
+Spectral4= c("#4f0213","#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2","#1d1d51")
+Spectral2= c("#4f0213","#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#fffcdc","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2","#1d1d51")
+Spectral3= c("#9e0142","#d53e4f","#f46d43","#fdd7b1","#fffeeb","#beddba","#66c2a5","#3288bd","#5e4fa2")
+Spectral5= c("#4f0213","#9e0142","#d53e4f","#f46d43","#fdd7b1","#fffeeb","#beddba","#66c2a5","#3288bd","#5e4fa2","#251a56")
+Spectral6= c("#67001f","#d53e4f","#f46d43","#f4a582","#fdd7b1","#fffeeb","#beddba","#66c2a5","#3288bd","#5e4fa2","#251a56")
+Spectral7= c("#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#fffcdc","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2")
+lol=c("#FFFFFA", "#FCFFC9", "#ECD382" ,"#E7BA5E" ,"#E1A039" ,"#DA840B" ,"#CC691E" ,"#B45139" ,"#983B40" ,"#79273D" ,"#551531" ,"#410E28","#1D0B14","#0C0308")
+lol2=c("#ECD382" ,"#E7BA5E" ,"#E1A039" ,"#DA840B" ,"#CC691E" ,"#B45139" ,"#983B40" ,"#79273D")
+BlYe=c("#181831","#273068","#2e4c9b","#c77996","#f49c6b","#fdc835","#f1e95e")
+RdBl=c('#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#fefefe','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061')
+RdBl_mod=       c('#67001f','#b2182b','#d6604d','#f4a582','#fefefe','#92c5de','#4393c3','#2166ac','#053061')
+Heat=c('#ffffff','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026')
+RdBl_mod2=c('#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#fefefe',"#ffffff",'#fefefe','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061')
+RdBl_mod3=c('#67001f','#b2182b',"#f4a582",'#fddbc7',"#ffffff",'#d1e5f0','#92c5de','#2166ac','#053061')
+RdBl_mod4=c('#b2182b','#d6604d','#f4a582','#fddbc7','#fefefe',"#ffffff",'#fefefe','#d1e5f0','#92c5de','#4393c3','#2166ac')
+RdBl_mod5=c('#b2182b','#d6604d','#f4a582','#fddbc7',"#ffffff",'#d1e5f0','#92c5de','#4393c3','#2166ac')
+RdBl_mod6=c('#8f001f','#b2182b','#d6604d','#f4a582','#fddbc7',"#ffffff",'#d1e5f0','#92c5de','#4393c3','#2166ac','#00518f')
+RdBl_mod7=c('#b2182b','#d6604d','#f4a582','#fddbc7','#fefefe',"#ffffff",'#fefefe','#d1e5f0','#92c5de','#4393c3','#2166ac')
+x=c('#f4a582','#fddbc7','#fefefe',"#ffffff")
+x1=c(rev(x),c("#fdae61","#fee08b","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2"))
+google=c("#E22B29", "#F7B00C", "#2D9C41", "#346EF2", "#AAAAAA")
+
+RdBlck=paletteer::paletteer_c("ggthemes::Classic Red-Black", 200)
+
+Blues2=c("#FCFCFC", "#DCE3F1", "#BDCAE6" ,"#9CB2DA" ,"#7A9BCF" ,"#5484C5" ,"#366EAE" ,"#2A588D" ,"#1F436C" ,"#152F4D" ,"#0C1C30", "#040404")
+Rd=       c('#67001f','#b2182b','#d6604d','#f4a582')
+BlRdDark=c("#332874", "#6B579B" , "#B2A7D2" ,"#ffffff", "#CBAEA8" , "#955D56" ,"#621E1E")
+BlDark=c("#332874", "#6B579B" , "#B2A7D2" ,"#ffffff")
+RdDark=c("#621E1E", "#955D56" , "#CBAEA8")
+BlRdx3=paletteer::paletteer_c("grDevices::Blue-Red 3", 50)
+BlRdx=paletteer::paletteer_c("grDevices::Blue-Red", 50)
