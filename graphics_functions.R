554929a484e3787a209facc5e97c0dd95e7e7066
YevhenAkimov <52507019+YevhenAkimov@users.noreply.github.com>
2025-07-28 11:51:43 +0300

    Update graphics_functions.R

--- graphics_functions.R
+++ graphics_functions.R
@@ -234,7 +234,7 @@ ggscatter_colored <- function(coords,
                               show_high_text   = TRUE,
                               symmQuant        = NULL,
                               dimnamesXYZ      = NULL,
-                              gg_theme = ggplot2::theme_light()
+                              gg_theme = NULL
                              ) {
   if (is.null(colors)) {
     colors = c("#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2")
@@ -369,10 +369,14 @@ ggscatter_colored <- function(coords,
         )
     }
     
-    plot_ <- plot_ + gg_theme()
+    plot_ <- plot_ 
   }
+  if (is.null(gg_theme) {
+    return(plot_)
+  } else {
+    return(plot_+gg_theme())
+    }
   
-  plot_
 }
 
 
